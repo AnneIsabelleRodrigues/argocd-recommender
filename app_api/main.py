b2951f4659bc002fc9ae5de9b9005cb79c95325d
annecarvalho <anneisabelle.rc@gmail.com>
2023-11-24 02:43:36 +0000

    first commit

---
+++ app_api/main.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import pandas as pd
+from flask import jsonify
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+# class PlaylistBase(BaseModel):
+#     model_date: str
+#     playlist_ids: List[int]
+#     version: str
+
+
+# class Songs(BaseModel):
+#     songs: List[str]
+
+@app.route('/api/recommend', methods=['POST'])
+def recommend():
+    songs = request.get_json()
+
+    model = pd.read_pickle("../../rules.pkl")
+    data = pd.read_pickle("../../data.pkl")
+
+    musics = []
+    pids = []
+
+    for song in songs['songs']:
+        model['antecedents'].apply(lambda x: musics.append(x) if song in x else None)
+
+    for setlist in musics:
+        lset = list(setlist)
+        for index, row in data.iterrows():
+            if set(lset).issubset(set(row['track_name'])):
+                pids.append(row['pid'])
+
+    pids = list(set(pids))
+
+    now = datetime.now()
+
+    date = now.strftime("%Y-%m-%d")
+
+    return {
+        "model_date": date,
+        "playlist_ids": pids,
+        "version": "1.0"
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app_ml/fp-growth.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import fpgrowth, association_rules
+
+file_path1 = '/home/datasets/2023_spotify_ds1.csv'
+file_path2 = '/home/datasets/2023_spotify_ds2.csv'
+file_paths = '/home/datasets/2023_spotify_songs.csv'
+
+playlist = pd.read_csv(file_path1, header=0)
+playlist2 = pd.read_csv(file_path2, header=0)
+songs = pd.read_csv(file_paths, header=0)
+
+playlist = pd.concat([playlist, playlist2], ignore_index=True)
+
+songs.reset_index(inplace=True)
+result = pd.merge(playlist, songs, on='track_name')
+
+result['track_name'] = result['track_name'].apply(lambda x: [str(x)])
+data = result.groupby(['pid'])['track_name'].agg("sum")
+data = pd.DataFrame(data)
+data.dropna(inplace=True)
+data.reset_index(inplace=True)
+
+transactions = data['track_name'].tolist()
+data['pid'] = data['pid'].astype(int)
+
+transaction_encoder = TransactionEncoder()
+transaction_array = transaction_encoder.fit(transactions).transform(transactions)
+transaction_dataframe = pd.DataFrame(transaction_array, columns=transaction_encoder.columns_)
+itemsets=fpgrowth(transaction_dataframe,min_support=0.02, use_colnames=True)
+itemsets.sort_values("support", ascending=False)
+
+association_rules_df = association_rules(itemsets, metric="confidence", min_threshold=.50)
+
+association_rules_df = association_rules_df[(association_rules_df["support"]>0.05) & (association_rules_df["confidence"]>0.1) & (association_rules_df["lift"]>5)].sort_values("confidence", ascending=False)
+
+association_rules_df.to_pickle("./rules.pkl")
+data.to_pickle("./data.pkl")
